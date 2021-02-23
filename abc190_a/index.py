5af7f862792c9b43c27dcd91a9faf344bc250abd
t29mato <tomoya.matou@gmail.com>
2021-02-24 08:56:11 +0900

    ref: hajima-san

--- abc190_a/index.py
+++ abc190_a/index.py
@@ -4,3 +4,6 @@ if C == 1: # Aoki君が選手
     print('Aoki' if A < B else 'Takahashi')
 else:
     print('Takahashi' if A > B else 'Aoki')
+
+# 勝敗が分かれるのは、高橋君と青木君の飴の数が同じ時だけなので、
+# その時だけ、先手がどちらかを見て決める回答でも良い。（haijimaさんの回答がそうなってた）
